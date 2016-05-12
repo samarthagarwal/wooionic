7e08f6863f19c0dce1193bf03ef2cb8c28c029bc
Samarth Agarwal <samarthagarwal@live.com>
2016-05-12 23:05:15 +0530

    added ionic refresher and infinite scroll

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -24,8 +24,8 @@ angular.module('starter.controllers', [])
 })
 
 .controller('BrowseCtrl', function($scope, WC){
-  $scope.offset = 0;
   
+  $scope.offset = 0;
   
   $scope.getProducts = function(){
     var Woocommerce = WC.WC();
@@ -37,9 +37,8 @@ angular.module('starter.controllers', [])
       console.log(JSON.parse(res));
       
       $scope.products = JSON.parse(res).products;
-      
-      $scope.canLoadMore = true;
       $scope.offset = $scope.offset + 10;
+      $scope.canLoadMore = true;
     })
   }
   
@@ -52,18 +51,29 @@ angular.module('starter.controllers', [])
   
   $scope.loadMore = function(){
     
-    Woocommerce = WC.WC();
+    var Woocommerce = WC.WC();
     
     Woocommerce.get('products?filter[offset]='+$scope.offset, function(err, data, res){
+      
       if(err)
         console.log(err);
         
-     JSON.parse(res).products.forEach(function(element, index){
-       $scope.products.push(element);
-     });
-     
-     $scope.$broadcast('scroll.infiniteScrollComplete');
+      JSON.parse(res).products.forEach(function(element, index){
+        
+        $scope.products.push(element);
+        
+      })
       
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+      
+      if(JSON.parse(res).products.length < 10){
+        $scope.canLoadMore = false;
+        console.log("no more products!");
+        return;
+      }
+      else{
+        $scope.offset = $scope.offset + 10;
+      }
       
     })
     
