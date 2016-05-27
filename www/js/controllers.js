34b5d2362964aa815acfd008d6abf3d3b86a4f05
Samarth Agarwal <samarthagarwal@live.com>
2016-05-27 16:57:17 +0530

    signup code completed

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -254,7 +254,137 @@ angular.module('starter.controllers', [])
   
 })
 
-.controller('SignUpCtrl', function(){
+.controller('SignUpCtrl', function($scope, $ionicPopup, $state, WC){
+  
+  $scope.newUser = {};
+  $scope.newUser.isValid = true;
+  
+  $scope.checkUserEmail = function(email){
+    
+    var regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    if(!regex.test(email)){
+      
+      $scope.newUser.isValid = false;
+      
+      $ionicPopup.show({
+        template: "<center>Invalid Email! Please Check!</center>",
+        buttons: [{
+          text: 'OK'
+        }]
+      });
+      
+      return;
+      
+    }
+    
+    
+    var Woocommerce = WC.WC();
+    
+    Woocommerce.get('customers/email/'+email,  function(err, data, res){
+      
+      if(err)
+        console.log(err);
+        
+      if(JSON.parse(res).customer){
+        
+        $scope.newUser.isValid = false;
+        
+        $ionicPopup.show({
+          template: "<center>EMail is already registered. Please login or use another email address.</center>",
+          buttons: [{
+            text: "Login",
+            onTap: function(e){
+              $state.go("app.login");
+            }
+          },{
+            text: "OK"
+          }]
+        })
+        
+      }
+      else{
+        $scope.newUser.isValid = true;
+      }
+      
+    })
+    
+  }
+  
+  
+  $scope.switchBillingToShipping = function(){
+    $scope.newUser.shipping_address = $scope.newUser.billing_address;
+  }
+  
+  
+  $scope.signUp = function(newUser){
+    
+    var customerData = {};
+    
+    customerData.customer = {
+        "email": newUser.email,
+        "first_name": newUser.first_name,
+        "last_name": newUser.last_name,
+        "username": newUser.email.split("@")[0],
+        "password": newUser.password,
+        "billing_address": {
+          "first_name": newUser.first_name,
+          "last_name": newUser.last_name,
+          "company": "",
+          "address_1": newUser.billing_address.address_1,
+          "address_2": newUser.billing_address.address_2,
+          "city": newUser.billing_address.city,
+          "state": newUser.billing_address.state,
+          "postcode": newUser.billing_address.postcode,
+          "country": newUser.billing_address.country,
+          "email": newUser.email,
+          "phone": newUser.billing_address.phone
+        },
+        "shipping_address": {
+          "first_name": newUser.first_name,
+          "last_name": newUser.last_name,
+          "company": "",
+          "address_1": newUser.shipping_address.address_1,
+          "address_2": newUser.shipping_address.address_2,
+          "city": newUser.shipping_address.city,
+          "state": newUser.shipping_address.state,
+          "postcode": newUser.shipping_address.postcode,
+          "country": newUser.shipping_address.country
+        }
+      }
+    
+    var Woocommerce = WC.WC();
+    
+    Woocommerce.post('customers', customerData, function(err, data, res){
+      if(err)
+        console.log(err);
+        
+      if(JSON.parse(res).customer){
+        $ionicPopup.show({
+          title: "Congratulations",
+          template: "Tour account has been created successfully. Please login.",
+          buttons: [{
+            text: "Login",
+            type: "button-assertive",
+            onTap: function(e){
+              $state.go('app.login');
+            }
+          }]
+        })
+      }
+      else{
+        $ionicPopup.show({
+          title: "OOPS",
+          template: JSON.parse(res).errors[0].message,
+          buttons: [{
+            text: "OK",
+            type: "button-assertive"
+          }]
+        })
+      }
+    });
+    
+  }
   
   
 })
